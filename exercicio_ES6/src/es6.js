804ce1306f6b818ae0a2d13ef1bf73ae2cbbd367
rpatetti <patetti.rafael@gmail.com>
2024-05-22 20:01:28 -0300

    adicionado exercicio_es6

---
+++ exercicio_ES6/src/es6.js
@@ -0,0 +1,15 @@
+const alunos = [
+    { nome: 'João', nota: 7 },
+    { nome: 'Maria', nota: 5 },
+    { nome: 'Ana', nota: 8 },
+    { nome: 'Pedro', nota: 4 },
+    { nome: 'Lucas', nota: 6 },
+    { nome: 'Laura', nota: 9 }
+];
+
+function filtraAprovados(alunos) {
+    return alunos.filter(aluno => aluno.nota >= 6);
+}
+
+const aprovados = filtraAprovados(alunos);
+console.log(aprovados);
